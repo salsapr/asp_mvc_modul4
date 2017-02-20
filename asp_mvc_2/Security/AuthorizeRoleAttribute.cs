b6d1ec382ca52e15c17dce8ec1e154e5f88c6c02
salsapr <salsabilaputrirayieningtyas@gmail.com>
2017-02-20 08:55:24 +0700

    ASP MVC2 Modul 4

---
+++ asp_mvc_2/Security/AuthorizeRoleAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using asp_mvc_2.Models.DB;
+using asp_mvc_2.Models.EntityManager;
+
+namespace asp_mvc_2.Security
+{
+    public class AuthorizeRoleAttribute : AuthorizeAttribute
+    {
+        private readonly string[] userAssignedRoles;
+        public AuthorizeRoleAttribute(params string[] roles)
+        {
+            this.userAssignedRoles = roles;
+        }
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            bool authorize = false;
+            using (DemoDBEntities2 db = new DemoDBEntities2())
+            {
+                UserManager UM = new UserManager();
+                foreach (var roles in userAssignedRoles)
+                {
+                    authorize = UM.IsUserInRole(httpContext.User.Identity.Name, roles);
+                    if (authorize)
+                        return authorize;
+                }
+            }
+            return authorize;
+        }
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("~/Home/UnAuthorized");
+        }
+    }
+}
